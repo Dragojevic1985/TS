ac51d4cd5cd1f3c822e4163ff10396d17f74c764
Boban Dragojevic <boban.dragojevic@yahoo.com>
2021-05-30 22:48:44 +0200

    Entity Framework

---
+++ TextStatistics/SQLiteScripts/Alter_NumberOfLines_OnBook.sql
@@ -0,0 +1,3 @@
+﻿-- Script Date: 5/30/2021 10:40 PM  - ErikEJ.SqlCeScripting version 3.5.2.87
+-- Adding as column with NOT NULL is not allowed, set a default value or allow NULL
+ALTER TABLE [Book] ADD COLUMN [NumberOfLines] INTEGER NOT NULL;
---
+++ TextStatistics/SQLiteScripts/Alter_NumberOfWords_OnBook.sql
@@ -0,0 +1,2 @@
+﻿-- Script Date: 5/30/2021 10:38 PM  - ErikEJ.SqlCeScripting version 3.5.2.87
+ALTER TABLE [Book] ADD COLUMN [NumberOfWords] INTEGER NULL;
